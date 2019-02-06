b69f0f404640d507df9b92b527810f098104a028
Prudvi <prudvi@users.noreply.github.com>
2019-02-06 18:21:43 +0530

    modified the Book Component

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
 
+import { BookServiceService } from './../services/book-service.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { BookModel } from '../model/book-model';
+import { Subscription,Observable, of } from 'rxjs';
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
   styleUrls: ['./books.component.css']
 })
-export class BooksComponent implements OnInit {
+export class BooksComponent implements OnInit,OnDestroy  {
 
-  constructor() { }
+  bookList: BookModel[] = [] ;
 
-  ngOnInit() {
+	bkListSubscibe: Subscription;
+	bkDeletetSubscibe: Subscription;
+	constructor(private bks :BookServiceService) { }
+
+	ngOnInit() {
+		this.getBooks();
   }
+  
+  getBooks() {
+		this.bookList = [];
+		this.bkListSubscibe = this.bks.getBooks().subscribe((data: BookModel[]) => {
+      
+      this.bookList = data;
+      console.log(":::"+this.bookList);
+		});
+	}
 
+  ngOnDestroy() {
+		//this.emListSubscibe.unsubscribe();
+		//this.emDeletetSubscibe.unsubscribe();
+	}
 }
