3fe87b626aa2770cf85a918930d2212536df5438
Prudvi <prudvi@users.noreply.github.com>
2019-02-11 07:56:28 +0530

    Module File changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,23 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { BodyComponent } from './body/body.component';
 
-import {ApplicationRoutesModule} from './application-routes/application-routes.module'
-
+import {ApplicationRoutesModule} from './application-routes/application-routes.module';
+import { StoreModule } from '@ngrx/store';
 
 import { HttpClientModule  } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule, NgbActiveModal  } from '@ng-bootstrap/ng-bootstrap';
+
 import { from } from 'rxjs';
 import { BooksComponent } from './books/books.component';
 import { MoviesComponent } from './movies/movies.component';
 import { BooksListComponent } from './books/books-list/books-list.component';
-import { RatingBooksModuleModule } from './rating-books-module/rating-books-module.module'
+import { RatingBooksModuleModule } from './rating-books-module/rating-books-module.module';
+import { AddNewBookComponent } from './books/add-books/add-new-book/add-new-book.component';
+import { DeleteConfirmationComponent } from './common/delete-confirmation/delete-confirmation.component';
+
+import { reducer } from '../app/books/store/reducers/book.reducers';
 
 @NgModule({
   declarations: [
@@ -27,13 +32,18 @@ import { RatingBooksModuleModule } from './rating-books-module/rating-books-modu
     BodyComponent,
     BooksComponent,
     MoviesComponent,
-    BooksListComponent
+    BooksListComponent,
+    AddNewBookComponent,
+    DeleteConfirmationComponent
   ],
   imports: [
-    BrowserModule,HttpClientModule,FormsModule,NgbModule,
-    ApplicationRoutesModule,RatingBooksModuleModule
+    BrowserModule, HttpClientModule, FormsModule, NgbModule,
+    ApplicationRoutesModule, RatingBooksModuleModule, 
+    StoreModule.forRoot({bookList: reducer})
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [NgbActiveModal ],
+  bootstrap: [AppComponent],
+  entryComponents: [DeleteConfirmationComponent],
+
 })
 export class AppModule { }
